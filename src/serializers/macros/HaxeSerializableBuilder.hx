51a03f1f6f66423c61165c2964ab7bcfd47c64cd
Ian Harrigan <ianharrigan@hotmail.com>
2024-05-17 13:27:30 +0200

    better haxe serialization

--- src/serializers/macros/HaxeSerializableBuilder.hx
+++ src/serializers/macros/HaxeSerializableBuilder.hx
@@ -5,13 +5,27 @@ import haxe.macro.Expr;
 
 class HaxeSerializableBuilder {
     public static macro function build():Array<Field> {
-        var hasSuper:Bool = Context.getLocalClass().get().superClass != null;
+        Sys.println(" - adding haxe serialization to " + Context.getLocalClass().toString());
 
+        var config = SerializableBuilder.getConfig(Context.getLocalClass().get().meta);
+        var fieldNames:Array<String> = []; // just hold onto these so we can easily check if ignore list contains something that doesnt exist
         var fields = Context.getBuildFields();
+        for (f in fields) {
+            fieldNames.push(f.name);
+        }
+        for (ignore in config.ignore) {
+            if (!fieldNames.contains(ignore)) {
+                Context.warning('serializer settings reference a field to ignore that doesnt exist: ${ignore}', Context.currentPos());
+            }
+        }
+
+        var hasSuper:Bool = Context.getLocalClass().get().superClass != null;
         SerializableBuilder.findOrAddConstructor(fields, hasSuper);
 
         var serializeFn = SerializableBuilder.findOrAddSerialize(fields, hasSuper);
+        var serializeImplFn = findOrAddSerializeImpl(fields, hasSuper);
         var unserializeFn = SerializableBuilder.findOrAddUnserialize(fields, hasSuper);
+        var unserializeImplFn = findOrAddUnserializeImpl(fields, hasSuper);
         
         var localClass = Context.getLocalClass();
         var parts = localClass.toString().split(".");
@@ -22,14 +36,70 @@ class HaxeSerializableBuilder {
             name: s
         });
 
+        switch (serializeImplFn.kind) {
+            case FFun(f): {
+                switch (f.expr.expr) {
+                    case EBlock(exprs):
+                        if (hasSuper) {
+                            exprs.push(macro super.serializeImpl(serializer));
+                        }
+
+                        for (f in fields) {
+                            if (f.access.contains(AStatic)) {
+                                continue;
+                            }
+                            if (config.ignore.contains(f.name)) {
+                                continue;
+                            }
+                            switch (f.kind) {
+                                case FVar(t, e):
+                                    var fieldName = f.name;
+                                    exprs.push(macro serializer.serialize($i{fieldName}));
+                                case _:   
+                            }
+                        }
+                    case _:    
+                }
+            }
+            case _:
+        }
+
         switch (serializeFn.kind) {
             case FFun(f): {
                 switch (f.expr.expr) {
                     case EBlock(exprs):
                         exprs.push(macro var serializer = new haxe.Serializer());
                         exprs.push(macro serializer.useCache = true);
-                        exprs.push(macro serializer.serialize(this));
+                        exprs.push(macro serializeImpl(serializer));
                         exprs.push(macro return serializer.toString());
+                    case _:    
+                }
+            }
+            case _:
+        }
+
+        switch (unserializeImplFn.kind) {
+            case FFun(f): {
+                switch (f.expr.expr) {
+                    case EBlock(exprs):
+                        if (hasSuper) {
+                            exprs.push(macro super.unserializeImpl(unserializer));
+                        }
+
+                        for (f in fields) {
+                            if (f.access.contains(AStatic)) {
+                                continue;
+                            }
+                            if (config.ignore.contains(f.name)) {
+                                continue;
+                            }
+                            switch (f.kind) {
+                                case FVar(t, e):
+                                    var fieldName = f.name;
+                                    exprs.push(macro $i{fieldName} = unserializer.unserialize());
+                                case _:   
+                            }
+                        }
                     case _:    
                 }
             }
@@ -40,26 +110,11 @@ class HaxeSerializableBuilder {
             case FFun(f): {
                 switch (f.expr.expr) {
                     case EBlock(exprs):
-                        if (hasSuper) {
-                            exprs.push(macro super.unserialize(data));
-                        }
                         exprs.push(macro if (!(data is String)) { // if the response isnt a string, lets turn it into one
                             data = Std.string(data);
                         });
                         exprs.push(macro var unserializer = new haxe.Unserializer(data));
-                        exprs.push(macro var parsedData = unserializer.unserialize());
-
-                        for (field in fields) {
-                            if (field.access.contains(AStatic)) {
-                                continue;
-                            }
-                            switch (field.kind) {
-                                case FVar(t, e):
-                                    var fieldName = field.name;
-                                    exprs.push(macro this.$fieldName = parsedData.$fieldName);
-                                case _:    
-                            }
-                        }
+                        exprs.push(macro unserializeImpl(unserializer));
                     case _:    
                 }
             }
@@ -67,5 +122,65 @@ class HaxeSerializableBuilder {
         }
 
         return fields;
+    }
+
+    public static function findOrAddSerializeImpl(fields:Array<Field>, hasSuper:Bool):Field {
+        var fn:Field = null;
+        for (field in fields) {
+            if (field.name == "serializeImpl") {
+                fn = field;
+            }
+        }
+        
+        var access = [APrivate];
+        if (hasSuper) {
+            access.push(AOverride);
+        }
+
+        if (fn == null) {
+            fn = {
+                name: "serializeImpl",
+                access: access,
+                kind: FFun({
+                    args:[{name: "serializer", type: macro: haxe.Serializer}],
+                    expr: macro {
+                    }
+                }),
+                pos: Context.currentPos()
+            }
+            fields.push(fn);
+        }
+
+        return fn;
+    }
+
+    public static function findOrAddUnserializeImpl(fields:Array<Field>, hasSuper:Bool):Field {
+        var fn:Field = null;
+        for (field in fields) {
+            if (field.name == "unserializeImpl") {
+                fn = field;
+            }
+        }
+        
+        var access = [APrivate];
+        if (hasSuper) {
+            access.push(AOverride);
+        }
+
+        if (fn == null) {
+            fn = {
+                name: "unserializeImpl",
+                access: access,
+                kind: FFun({
+                    args:[{name: "unserializer", type: macro: haxe.Unserializer}],
+                    expr: macro {
+                    }
+                }),
+                pos: Context.currentPos()
+            }
+            fields.push(fn);
+        }
+
+        return fn;
     }
 }
